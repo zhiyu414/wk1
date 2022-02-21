25acdf80008c7af8d7a7412f78cc0e33f5e9f81e
zhiyu <910414tiffany@gmail.com>
2022-02-21 13:29:38 +0800

    2st

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.c}>٩(๑•̀ω•́๑)۶</Text>
+      <Text style={styles.a}>110919018</Text>
+      <Text style={styles.b}>林芷伃</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e7ded6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:100,
   },
+  a:{
+    fontSize:50,
+    color:'#92796f',
+  },
+  b:{
+    fontSize:30,
+    color:'#595959',
+  },
+  c:{
+    fontSize:50,
+    color:'#a8a8a8',
+  }
 });
